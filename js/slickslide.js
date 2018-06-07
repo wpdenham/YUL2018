2da9638c470fef1179cdb29bca33b6904edf05d9
wpdenham <wpdenham@gmail.com>
2018-06-06 20:46:54 -0500

    Began adding a slider for the experiences page. Additional styling and markup added

---
+++ js/slickslide.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slides').slick({
+  	dots: false,
+  	speed: 2000,
+  	autoplay: true,
+  	autoplaySpeed: 4000,
+  	fade: true,
+  	pauseOnHover: false,
+  	pauseOnFocus: false,
+  	cssEase: 'ease-in-out'
+  });
+});
