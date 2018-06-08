ea3d751f145330e16b591b6ec4d3fed5c74e3f09
wpdenham <wpdenham@gmail.com>
2018-06-07 21:33:57 -0500

    Styling and content added for Experiences Page.

--- js/slickslide.js
+++ js/slickslide.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   $('.slides').slick({
   	dots: false,
+    arrows: false,
   	speed: 2000,
   	autoplay: true,
   	autoplaySpeed: 4000,
   	fade: true,
   	pauseOnHover: false,
   	pauseOnFocus: false,
-  	cssEase: 'ease-in-out'
+  	cssEase: 'ease-in-out',
+    infinite: true,
   });
 });
